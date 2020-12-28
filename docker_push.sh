8d04aeec0abbdb8fa582ceb7a300eb166b5ed90d
Stefan Manevski <stefanmanevski12@gmail.com>
2020-12-28 09:59:14 +0100

    bump docker version

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag bouncing-balls smanevski/bouncing-balls:1.2.0
-docker push smanevski/bouncing-balls:1.2.0
+docker tag bouncing-balls smanevski/bouncing-balls:1.3.0
+docker push smanevski/bouncing-balls:1.3.0
